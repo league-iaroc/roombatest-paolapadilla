1c12e0ef57cc591ded05ce22408a818f5da12704
CeilaScarlet <league@Admins-Mac-mini.local>
2017-06-10 11:28:00 -0700

    i learn how to move a robot to the the green dot.

--- src/level1/Challenge.java
+++ src/level1/Challenge.java
@@ -12,18 +12,32 @@ public class Challenge extends Head {
 	public Challenge(Roomba roomba) {
 		super(roomba);
 	}
+
 	/**
-	 * Without touching the wall reach the endZone
-	 * use sleep an driveDirect 
+	 * Without touching the wall reach the endZone use sleep an driveDirect
 	 */
 	public void initialize() {
-		//write code here!
-		
-		
-		
+		// write code here!
+		driveDirect(300, 300);
+		sleep(3000);
+		driveDirect(100, 200);
+		sleep(3000);
+		driveDirect(300,300);
+		sleep(4000);
+		driveDirect(400,600);
+		sleep(1000);
+		driveDirect(300,200);
+		sleep(1000);
+		driveDirect(300,300);
+		sleep(1000);
+		driveDirect(300,300);
+		sleep(3000);
+		driveDirect(100,200);
+		sleep(3000);
+		driveDirect(300,300);
 	}
 
 	public void loop() {
-	
+
 	}
 }
